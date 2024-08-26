ca7892b60aeb91e455341ee23ff085ee420fe941
Gustavo <gusstavocta@gmail.com>
2024-08-26 11:20:30 -0500

    TODO

--- src/useReducer/intro-reducer.js
+++ src/useReducer/intro-reducer.js
@@ -1,1 +1,30 @@
-console.log('intro-reducer.js');
+const initialState = [{
+    id:1,
+    todo: 'Recolectar la piedra alma',
+    done: false,
+}];
+
+
+const todoReducer = (state = initialState, action={}) => {
+    if(action.type === '[TODO] add todo'){
+        return [...state, action.payload];
+    }
+
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id:2,
+    todo: 'Derrotar a Thanos',
+    done: false,
+}
+
+const addTodoAction = {
+    type: '[TODO] add todo',
+    payload: newTodo,
+}
+todos = todoReducer(todos,addTodoAction);
+
+console.log({state: todos}); // [{id:1, todo: 'Recolectar la piedra alma', done: false}]
