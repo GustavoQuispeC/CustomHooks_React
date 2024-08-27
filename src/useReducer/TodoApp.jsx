2fbb235a187844e94598b0e2ec7ed7e2d70c8ef4
Gustavo <gusstavocta@gmail.com>
2024-08-27 01:12:56 -0500

    todoApp complete

--- src/useReducer/TodoApp.jsx
+++ src/useReducer/TodoApp.jsx
@@ -1,30 +1,34 @@
-
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd";
 import { useTodo } from "../hooks/useTodo";
 
-
-
-
 const TodoApp = () => {
-  const { todos,  todosCount, pendingTodosCount, HandleDeleteTodo,HandleToggleTodo,HandleNewTodo } = useTodo();
-  
+  const {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    HandleDeleteTodo,
+    HandleToggleTodo,
+    HandleNewTodo,
+  } = useTodo();
 
   return (
     <>
-       <h1>TodoApp: { todosCount }, <small>pendientes: { pendingTodosCount }</small> </h1>
+      <h1>
+        TodoApp: {todosCount}, <small>pendientes: {pendingTodosCount}</small>{" "}
+      </h1>
       <hr />
       <div className="row">
         <div className="col-7">
-          <TodoList 
-          todos={todos} 
-          onDeleteTodo={HandleDeleteTodo}
-          onToggleTodo={HandleToggleTodo}
+          <TodoList
+            todos={todos}
+            onDeleteTodo={HandleDeleteTodo}
+            onToggleTodo={HandleToggleTodo}
           />
         </div>
 
         <div className="col-5">
-          <h4>Agregar TODO</h4>
+          <h4>Agregar un nuevo ToDo</h4>
           <hr />
           <TodoAdd onNewTodo={HandleNewTodo} />
         </div>
