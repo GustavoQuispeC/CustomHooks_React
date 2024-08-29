24ae03c349b958e838c43c41bda083eb9eed227d
Gustavo <gusstavocta@gmail.com>
2024-08-28 23:26:52 -0500

    implementado UseContext

--- src/useContext/LoginPage.jsx
+++ src/useContext/LoginPage.jsx
@@ -1,8 +1,24 @@
+import { useContext } from "react";
+import { UserContext } from "./context/userContext";
+
+
 const LoginPage = () => {
+  const { user,setUser } = useContext(UserContext);
+
   return (
     <>
       <h1>Login Page</h1>
       <hr />
+      <pre>{JSON.stringify(user, null, 5)}</pre>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setUser({ id: 123, name: "Gustavo", email: "gustavo@gmail.com" });
+        }}
+      >
+        Establecer usuario
+      </button>
     </>
   );
 };
--- src/useContext/context/UserProvider.jsx
+++ src/useContext/context/UserProvider.jsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import { UserContext } from "./userContext";
 import PropTypes from "prop-types";
 
-const user = {
-  id: 123,
-  name: "Gustavo",
-  email: "gustavo@gmail.com",
-};
+// const user = {
+//   id: 123,
+//   name: "Gustavo",
+//   email: "gustavo@gmail.com",
+// };
 
 const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
   return (
-    <UserContext.Provider value={{ hola: "mundo", user: user }}>
+    <UserContext.Provider value={{user, setUser}}>
       {children}
     </UserContext.Provider>
   );
