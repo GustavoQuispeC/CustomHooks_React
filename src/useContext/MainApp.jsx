29655efc36f98a5232b968042ca1ab8fe1861627
Gustavo <gusstavocta@gmail.com>
2024-08-28 19:27:00 -0500

    implementado Link

--- src/useContext/MainApp.jsx
+++ src/useContext/MainApp.jsx
@@ -1,8 +1,29 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import LoginPage from "./LoginPage";
+import Navbar from "./Navbar";
+
+
 const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Navbar/>
+      
       <hr />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
+
+        {/* <Route path="*" element={<LoginPage/>} /> */}
+
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 };
